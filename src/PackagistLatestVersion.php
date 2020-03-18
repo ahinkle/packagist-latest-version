9904259a13dfa303e928662dfc3ac11982f99ac5
ahinkle <ahinkle10@gmail.com>
2020-03-18 17:14:30 -0500

    First Commit

--- src/PackagistLatestVersion.php
+++ src/PackagistLatestVersion.php
@@ -1,8 +1,126 @@
 <?php
 
-namespace Ahinkle\PackagistLatestVersion;
+namespace ahinkle\PackagistLatestVersion;
+
+use Exception;
+use GuzzleHttp\Client;
 
 class PackagistLatestVersion
 {
-    // Build your next great package.
+        /**
+         * The Guzzle Client.
+         *
+         * @var \GuzzleHttp\Client
+         */
+        protected $client;
+
+        /**
+         * The Packagist API.
+         *
+         * @var \Spatie\Packagist\Packagist
+         */
+        protected $packagist;
+
+        /**
+         * The latest version of the package.
+         *
+         * @var string|null
+         */
+        protected $latestVersion = null;
+
+        /**
+         * Release tags that are considered `developmental` releases
+         *
+         * @var array
+         */
+        protected $developmentalTags = [
+            'alpha',
+            'beta',
+            'dev',
+            'develop',
+            'development',
+            'master',
+            'rc',
+            'untagged',
+            'wip',
+        ];
+    
+        /**
+         * @param \GuzzleHttp\Client $client
+         * @param string             $baseUrl
+         */
+        public function __construct(Client $client, $baseUrl = 'https://packagist.org')
+        {
+            $this->client = $client;
+
+            $this->packagist = new \Spatie\Packagist\Packagist($client, $baseUrl);
+        }
+
+        /**
+         * The latest release of the specified package.
+         * 
+         * @param string $vendor
+         *
+         * @return string|null
+         */
+        public function getLatestRelease($package)
+        {
+            if ($package === '') {
+                throw new Exception('You must pass a package value');
+            }
+
+            $package = $this->packagist->getPackageMetaData($package);
+
+            if (! isset($package['package']['versions'])) {
+                return;
+            }
+
+            return $this->resolveLatestRelease($package['package']['versions']);
+        }
+
+        /**
+         * Resolves the latest release by the provided array.
+         *
+         * @param  array  $releases
+         * @return array|null
+         */
+        public function resolveLatestRelease($releases)
+        {
+            if (empty($releases)) {
+                return;
+            }
+
+            foreach ($releases as $release) {
+                if ($this->isDevelopmentalRelease($release['version_normalized'])) {
+                    continue;
+                }
+
+                if ($this->latestVersion) {
+                    if (version_compare($release['version_normalized'], $this->latestVersion['version_normalized'], '>')) {
+                        $this->latestVersion = $release;
+                    }
+                } else {
+                    $this->latestVersion = $release;
+                }
+            }
+
+            return $this->latestVersion;
+        }
+
+        /**
+         * If the the release tag is a developmental release.
+         *
+         * @param  string  $release
+         * @return bool
+         */
+        public function isDevelopmentalRelease($release)
+        {
+            foreach($this->developmentalTags as $developmentalTag) {
+                if (strpos($release, $developmentalTag) !== false) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 }
---
+++ tests/PackagistLatestVersionTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace ahinkle\PackagistLatestVersion\Tests;
+
+use ahinkle\PackagistLatestVersion\PackagistLatestVersion;
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+
+class PackagistLatestVersionTest extends TestCase
+{
+    /** @var ahinkle\PackagistLatestVersion\PackagistLatestVersion */
+    protected $packagist;
+
+    public function setUp()
+    {
+        $client = new Client();
+
+        $this->packagist = new PackagistLatestVersion($client);
+
+        parent::setUp();
+    }
+
+    public function test_it_can_instantiate_package_latest_version()
+    {
+        $this->assertInstanceOf(PackagistLatestVersion::class, $this->packagist);
+    }
+
+    public function test_it_can_get_latest_version()
+    {
+        $latest = $this->packagist->getLatestRelease('laravel/framework');
+
+        $this->assertIsArray($latest);
+        $this->assertArrayHasKey('version', $latest);
+    }
+
+    public function test_it_returns_null_when_empty_releases()
+    {
+        $this->assertNull($this->packagist->resolveLatestRelease([]));
+    }
+
+    public function test_it_returns_null_on_only_developmental_releases()
+    {
+        $this->assertNull($this->packagist->resolveLatestRelease([['version_normalized' => '0.1-dev']]));
+    }
+
+    public function test_it_resolves_latest_release()
+    {
+        $resolve = $this->packagist->resolveLatestRelease([
+            [
+                'version_normalized' => '1.0.0.0',
+                'version' => 'v1.0'
+            ]
+        ]);
+
+        $this->assertEquals('1.0.0.0', $resolve['version_normalized']);
+    }
+
+    public function test_it_properly_compares_latest_release()
+    {
+        $resolve = $this->packagist->resolveLatestRelease([
+            [
+                'version_normalized' => '1.2.0.0',
+                'version' => 'v1.2'
+            ],
+            [
+                'version_normalized' => '2.0.0.0',
+                'version' => 'v2.0'
+            ],
+            [
+                'version_normalized' => '1.0.0.0',
+                'version' => 'v1.0',
+            ]
+        ]);
+
+        $this->assertEquals('v2.0', $resolve['version']);
+    }
+
+    public function test_it_returns_proper_bool_on_is_dev_release()
+    {
+        $this->assertTrue($this->packagist->isDevelopmentalRelease('dev'));
+        $this->assertTrue($this->packagist->isDevelopmentalRelease('v0.01-dev'));
+        $this->assertTrue($this->packagist->isDevelopmentalRelease('dev-0.01'));
+        $this->assertTrue($this->packagist->isDevelopmentalRelease('dev-master'));
+        
+        $this->assertFalse($this->packagist->isDevelopmentalRelease('1.0.0.0'));
+        $this->assertFalse($this->packagist->isDevelopmentalRelease('1.2.3.4'));
+        $this->assertFalse($this->packagist->isDevelopmentalRelease('1.2.3.4-foo'));
+    }
+}
